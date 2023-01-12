1721225cdcca771db6f1d8726df4c592e3acbce3
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-01-12 10:19:47 -0600

    added SSRProvider component to application to remove warning

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,17 @@
 /* eslint-disable react/prop-types */
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { SSRProvider } from 'react-bootstrap';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <SSRProvider>
       <NavBar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </SSRProvider>
   );
 }
 
