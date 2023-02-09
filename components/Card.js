1cf173b6aaa5c893687e46c0d1f584a933913e4d
Adam Howard <howard.adamp@gmail.com>
2023-02-09 14:39:00 -0600

    setting up tailwinds config and updating card markup

--- components/Card.js
+++ components/Card.js
@@ -1,75 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Link from 'next/link';
-// import { BsGithub, BsLinkedin, BsGlobe2 } from 'react-icons/bs';
-// import { GrDocumentText } from 'react-icons/gr';
+import Link from 'next/link';
+import { BsGithub, BsLinkedin, BsGlobe2 } from 'react-icons/bs';
+import { GrDocumentText } from 'react-icons/gr';
 
 export default function TechieCard({ userObj }) {
   const {
-    linkedInUrl, githubUrl, portfolioUrl, resumeUrl,
-    // title, jobType, experienceLevel, lastName, firstName, imageUrl,
+    linkedInUrl, githubUrl, portfolioUrl, resumeUrl, imageUrl, lastName, firstName, title, jobType, experienceLevel,  
   } = userObj;
   const hasLinks = linkedInUrl || githubUrl || portfolioUrl || resumeUrl;
 
   return (
-    <>Card {hasLinks}</>
-    // <Card className="card" style={{ width: '18rem' }}>
-    //   <Card.Img src={imageUrl} className="card-img-top" height="200px" style={{ objectFit: 'cover' }} alt={`${firstName} ${lastName}`} />
-    //   <Card.Body>
-    //     <div className="card-content">
-    //       <Card.Title className="card-title">
-    //         {firstName} {lastName}
-    //       </Card.Title>
-    //       <Card.Text className="card-text">
-    //         <span className="card-text-title">Title:</span> {title}
-    //         <br />
-    //         <span className="card-text-jobType">Job Type:</span> {jobType}
-    //         <br />
-    //         <span className="card-text-experienceLevel">Experience Level:</span> {experienceLevel}
-    //       </Card.Text>
-    //     </div>
-    //     {hasLinks && (
-    //       <Card.Footer>
-    //         {linkedInUrl ? (
-    //           <Link href={linkedInUrl}>
-    //             <a target="_blank" href={linkedInUrl} rel="noreferrer">
-    //               <BsLinkedin className="card-icon" />
-    //             </a>
-    //           </Link>
-    //         ) : (
-    //           ''
-    //         )}
-    //         {githubUrl ? (
-    //           <Link href={githubUrl}>
-    //             <a target="_blank" href={githubUrl} rel="noreferrer">
-    //               <BsGithub className="card-icon" />
-    //             </a>
-    //           </Link>
-    //         ) : (
-    //           ''
-    //         )}
-    //         {portfolioUrl ? (
-    //           <Link href={portfolioUrl}>
-    //             <a target="_blank" href={portfolioUrl} rel="noreferrer">
-    //               <BsGlobe2 className="card-icon" />
-    //             </a>
-    //           </Link>
-    //         ) : (
-    //           ''
-    //         )}
-    //         {resumeUrl ? (
-    //           <Link href={resumeUrl}>
-    //             <a target="_blank" href={resumeUrl} rel="noreferrer">
-    //               <GrDocumentText className="card-icon" />
-    //             </a>
-    //           </Link>
-    //         ) : (
-    //           ''
-    //         )}
-    //       </Card.Footer>
-    //     )}
-    //   </Card.Body>
-    // </Card>
+    <>
+    <TechieCard className='bg-green-50'>
+      <TechieCard.Img src={imageUrl} className="" height="200px" style={{ objectFit: 'cover' }} alt={`${firstName} ${lastName}`} />
+      <TechieCard.Body>
+        <div>
+          <TechieCard.Title className="card-title">
+            {firstName} {lastName}
+          </TechieCard.Title>
+          <TechieCard.Text className="card-text">
+            <span className="card-text-title">Title:</span> {title}
+            <br />
+            <span className="card-text-jobType">Job Type:</span> {jobType}
+            <br />
+            <span className="card-text-experienceLevel">Experience Level:</span> {experienceLevel}
+          </TechieCard.Text>
+        </div>
+        {hasLinks && (
+          <TechieCard.Footer>
+            {linkedInUrl ? (
+              <Link href={linkedInUrl}>
+                <a target="_blank" href={linkedInUrl} rel="noreferrer">
+                  <BsLinkedin className="card-icon" />
+                </a>
+              </Link>
+            ) : (
+              ''
+            )}
+            {githubUrl ? (
+              <Link href={githubUrl}>
+                <a target="_blank" href={githubUrl} rel="noreferrer">
+                  <BsGithub className="card-icon" />
+                </a>
+              </Link>
+            ) : (
+              ''
+            )}
+            {portfolioUrl ? (
+              <Link href={portfolioUrl}>
+                <a target="_blank" href={portfolioUrl} rel="noreferrer">
+                  <BsGlobe2 className="card-icon" />
+                </a>
+              </Link>
+            ) : (
+              ''
+            )}
+            {resumeUrl ? (
+              <Link href={resumeUrl}>
+                <a target="_blank" href={resumeUrl} rel="noreferrer">
+                  <GrDocumentText className="card-icon" />
+                </a>
+              </Link>
+            ) : (
+              ''
+            )}
+          </TechieCard.Footer>
+        )}
+      </TechieCard.Body>
+    </TechieCard>
+    </>
   );
 }
 
