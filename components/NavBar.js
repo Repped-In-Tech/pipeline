fc00b5ec4cbbc97e84b2c7a618f0f6e30abdc27c
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-02-09 09:18:39 -0600

    removed bootstrap

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,33 +1,34 @@
-import Link from 'next/link';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+// import Link from 'next/link';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
 
 function NavBar() {
   return (
-    <Navbar bg="dark" fixed="top" variant="dark" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">PIPELINE</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Link passHref href="/">
-              <Nav.Link>All Talent</Nav.Link>
-            </Link>
-            <Link passHref href="/available-techies">
-              <Nav.Link>Available Talent</Nav.Link>
-            </Link>
-          </Nav>
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>Navbar</>
+    // <Navbar bg="dark" fixed="top" variant="dark" expand="lg">
+    //   <Container fluid>
+    //     <Navbar.Brand href="#">PIPELINE</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="navbarScroll" />
+    //     <Navbar.Collapse id="navbarScroll">
+    //       <Nav
+    //         className="me-auto my-2 my-lg-0"
+    //         style={{ maxHeight: '100px' }}
+    //         navbarScroll
+    //       >
+    //         <Link passHref href="/">
+    //           <Nav.Link>All Talent</Nav.Link>
+    //         </Link>
+    //         <Link passHref href="/available-techies">
+    //           <Nav.Link>Available Talent</Nav.Link>
+    //         </Link>
+    //       </Nav>
+    //       <Navbar.Text>
+    //         Signed in as: <a href="#login">Mark Otto</a>
+    //       </Navbar.Text>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
+// import FloatingLabel from 'react-bootstrap/FloatingLabel';
+// import Form from 'react-bootstrap/Form';
 import Card from '../components/Card';
 import techies from '../utils/techiesData';
 
 export default function Home() {
   return (
     <>
-      <FloatingLabel controlId="floatingTextarea" label="Search Techies" className="mb-3">
+      {/* <FloatingLabel controlId="floatingTextarea" label="Search Techies" className="mb-3">
         <Form.Control as="textarea" placeholder="Search Techies" />
-      </FloatingLabel>
+      </FloatingLabel> */}
       <div className="home">
         {techies.map((techie) => (
           <Card key={techie.id} userObj={techie} />
