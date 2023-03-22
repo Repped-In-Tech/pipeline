29217c7e7081c942148e1db3a4cb8b673cb9da3d
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-03-22 13:07:15 -0500

    fixed menu not showing on mobile

--- components/NavBar.js
+++ components/NavBar.js
@@ -157,49 +157,65 @@ export default function Navbar() {
             </div>
           </div>
 
-          { user && (
-            <Disclosure.Panel className="md:hidden">
-              <div className="border-t border-gray-700 pt-4 pb-3">
-                <div className="flex items-center px-5 sm:px-6">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src={user.photoURL}
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <div className="text-base font-medium text-white">
-                      {user.displayName}
-                    </div>
-                    <div className="text-sm font-medium text-gray-400">
-                      {user.email}
-                    </div>
-                  </div>
+          <Disclosure.Panel className="md:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium',
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+            { user && (
+            <div className="border-t border-gray-700 pt-4 pb-3">
+              <div className="flex items-center px-5 sm:px-6">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={user.photoURL}
+                    alt=""
+                  />
                 </div>
-                <div className="mt-3 space-y-1 px-2 sm:px-3">
-                  {userNavigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                  <Disclosure.Button
-                    key="sign-out"
-                    as="a"
-                    onClick={signOutUser}
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                  >
-                    Sign Out
-                  </Disclosure.Button>
+                <div className="ml-3">
+                  <div className="text-base font-medium text-white">
+                    {user.displayName}
+                  </div>
+                  <div className="text-sm font-medium text-gray-400">
+                    {user.email}
+                  </div>
                 </div>
               </div>
-            </Disclosure.Panel>
-          )}
+              <div className="mt-3 space-y-1 px-2 sm:px-3">
+                {userNavigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+                <Disclosure.Button
+                  key="sign-out"
+                  as="a"
+                  onClick={signOutUser}
+                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                >
+                  Sign Out
+                </Disclosure.Button>
+              </div>
+            </div>
+            )}
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
